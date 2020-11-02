fe83572548ba76b7c2a6d6d4377d4e35deb45c66
Kanilionis <kanilionis@gmail.com>
2020-11-02 16:16:53 -0700

    basic bones down

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Cheeseburger');
+INSERT INTO burgers (name) VALUES ('Bacon Burger');
+INSERT INTO burgers (name, devoured) VALUES ('Chicken Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Steak Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Double Cheeseburger', true);
+INSERT INTO burgers (name) VALUES ('Spicy Burger');
---
+++ public/assets/js/burgers.js
@@ -0,0 +1,60 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+  $(".change-sleep").on("click", function(event) {
+    var id = $(this).data("id");
+    var devoured = $(this).data("devoured");
+
+    var ifDevoured = {
+      devoured: devoured
+    };
+
+    // Send the PUT request.
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: ifDevoured
+    }).then(
+      function() {
+        console.log("changed eaten status to: ", ifDevoured);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+
+  $(".create-form").on("submit", function(event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+
+    var newBurger = {
+      name: $("#burger").val().trim(),
+      devoured: $("[name=devoured]:checked").val().trim()
+    };
+
+    // Send the POST request.
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(
+      function() {
+        console.log("created new burger");
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+
+  $(".delete-burger").on("click", function(event) {
+    var id = $(this).data("id");
+
+    // Send the DELETE request.
+    $.ajax("/api/burgers/" + id, {
+      type: "DELETE"
+    }).then(
+      function() {
+        console.log("deleted burger: ", id);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+});
