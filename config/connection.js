2a87bc6b01124262777aedbfc8541da13bff5af0
Kanilionis <kanilionis@gmail.com>
2020-11-03 20:11:34 -0700

    finished

--- config/connection.js
+++ config/connection.js
@@ -8,6 +8,17 @@ var connection = mysql.createConnection({
   password: "password",
   database: "burger_db"
 });
+if(process.env.JAWSDB_URL){
+  connection= mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: process.env.PORT || 3306,
+  user: "root",
+  password: "password",
+  database: "burgers_db",
+});
+}
 
 // Make connection.
 connection.connect(function(err) {
